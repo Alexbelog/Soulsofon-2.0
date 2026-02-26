cc7c251e1a07bb1e32a1b102e94131f1c2b6674f
Alexbelog <129123875+Alexbelog@users.noreply.github.com>
2026-02-26 03:23:28 +0300

    Update achievements.js

--- achievements.js
+++ achievements.js
@@ -370,12 +370,11 @@
     { id:"er_black_blade", img:"images/achievements/er_tarnished.png", name:"BLACK BLADE FALLEN",
       desc:"Elden Ring: победить Маликета, Чёрный клинок.", kind:"manual" },
     { id:"er_malenia",      img:"images/achievements/er_malenia.png",      name:"BLADE OF MIQUELLA", desc:"Elden Ring: победить Малению.", kind:"manual" },
+    { id:"er_sanya",      img:"images/achievements/er_sanya.png",      name:"SANYA DREAM", desc:"Помочь Александру, Железному кулаку испольнить его мечту!", kind:"manual" },
+    { id:"er_malenia",      img:"images/achievements/er_spirit_ashes.png",      name:"NO SPIRIT ASHES", desc:"Пройти игру полностью неиспользуя прах!", kind:"manual" },
     
     { id:"er_two_great_runes", img:"images/achievements/er_tarnished.png", name:"GREAT RUNES",
       desc:"Cобрать две Великие Руны и получить доступ в столицу.", kind:"manual" },
-    
-    { id:"er_two_great_runes", img:"images/achievements/er_tarnished.png", name:"SANYA DREAM",
-      desc:"Помочь Александру, Железному кулаку испольнить его мечту!", kind:"manual" },
 
     { id:"er_become_lord", img:"images/achievements/er_tarnished.png", name:"ELDEN LORD",
       desc:"Elden Ring: завершить игру любым финалом.", kind:"manual" },
